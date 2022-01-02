6eb41e2068926eaf61dd2988f5427e3cd0018e66
Arif0004 <arifmahmudaj6@gmail.com>
2022-01-02 09:52:07 +0600

    opp

---
+++ app/models/test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\models;
+
+class User
+{
+    function fromClass()
+    {
+        echo 'I am From ' . __CLASS__;
+    }
+}
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+//   "classmap": ["app/models"]
+
+require_once "./bootstrap.php";
+
+use App\Abc;
+use App\models\Another;
+
+
+// use app\models\User as custom;
+// use app\model\User as abc;
+
+// use app\models\Another;
+// $cs = new custom();
+// $cs->fromClass();
+// echo "<br>";
+// $ab = new abc();
+// $ab->fromClass();
+
+// new Another();
+// new Arif();
+
+ new Another();
+// new Abc();
